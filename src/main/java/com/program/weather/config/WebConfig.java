4bf5e2bd74743f31509720125734f823d9d11d40
Hoàng Ngọc Hưng <hoang.ngoc.hungit@gmail.com>
2019-10-28 23:49:23 +0700

    file_config

---
+++ src/main/java/com/program/weather/config/WebConfig.java
@@ -0,0 +1,65 @@
+package com.program.weather.config;
+
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+/**
+ * Configuration bean
+ * @author Ngoc Hung
+ *
+ */
+@Configuration
+public class WebConfig {
+
+	/**
+	 * Set locale for App is US
+	 * @return sessionLocaleResolver
+	 */
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+		sessionLocaleResolver.setDefaultLocale(Locale.US);
+		return sessionLocaleResolver;
+	}
+
+	/**
+	 * Configuration message
+	 * @return messageSource
+	 */
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+
+	/**
+	 * Configuration messageResource for Bean Validate
+	 * @param messageSource
+	 * @return
+	 */
+	@Bean
+	public LocalValidatorFactoryBean validator(MessageSource messageSource) {
+		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+		bean.setValidationMessageSource(messageSource);
+		return bean;
+	}
+
+	/**
+	 * Create bean RestTemplate
+	 * @return RestTemplate
+	 */
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+}
